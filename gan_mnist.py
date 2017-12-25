31593b8d1f601c091e11839fa627f92afcb61988
liudaizong <446880368@qq.com>
2017-12-25 09:20:29 +0800

    change

---
+++ gan_mnist.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.utils.data as Data
+import torchvision
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import numpy as np
+
+torch.manual_seed(1)    # reproducible
+
+# Hyper Parameters
+EPOCH = 64
+BATCH_SIZE = 100
+LR = 0.005         # learning rate
+DOWNLOAD_MNIST = False
+N_TEST_IMG = 5
+
+# Mnist digits dataset
+train_data = torchvision.datasets.MNIST(
+        root='./mnist/',
+        train=True,                                     # this is training data
+        transform=torchvision.transforms.ToTensor(),    # Converts a PIL.Image or numpy.ndarray to
+                                                        # torch.FloatTensor of shape (C x H x W) and normalize in the range [0.0, 1.0]
+        download=DOWNLOAD_MNIST,                        # download it if you don't have it
+)
+
+# Data Loader for easy mini-batch return in training, the image batch shape will be (50, 1, 28, 28)
+train_loader = Data.DataLoader(dataset=train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+# Discriminator
+D = nn.Sequential(
+    nn.Linear(784, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 1),
+    nn.Sigmoid())
+
+# Generator 
+G = nn.Sequential(
+    nn.Linear(64, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 256),
+    nn.LeakyReLU(0.2),
+    nn.Linear(256, 784),
+    nn.Tanh())
+
+criterion = nn.BCELoss()
+D_optimizer = torch.optim.Adam(D.parameters(), lr=0.001)
+G_optimizer = torch.optim.Adam(G.parameters(), lr=0.001)
+
+# initialize figure
+f, a = plt.subplots(2, N_TEST_IMG, figsize=(5, 2))
+plt.ion()   # continuously plot
+
+# original data (first row) for viewing
+view_data = Variable(train_data.train_data[:N_TEST_IMG].view(-1, 28*28).type(torch.FloatTensor)/255.)
+for i in range(N_TEST_IMG):
+    a[0][i].imshow(np.reshape(view_data.data.numpy()[i], (28, 28)), cmap='gray'); a[0][i].set_xticks(()); a[0][i].set_yticks(())
+
+for epoch in range(EPOCH):
+    for step, (x, y) in enumerate(train_loader):
+        images = Variable(x.view(-1, 28*28))   # batch x, shape (batch, 28*28)
+        b_y = Variable(x.view(-1, 28*28))   # batch y, shape (batch, 28*28)
+        b_label = Variable(y)               # batch label
+
+        # Create the labels which are later used as input for the BCE loss
+        real_labels = Variable(torch.ones(BATCH_SIZE))
+        fake_labels = Variable(torch.zeros(BATCH_SIZE))
+
+        #============= Train the discriminator =============#
+        # Compute BCE_Loss using real images where BCE_Loss(x, y): - y * log(D(x)) - (1-y) * log(1 - D(x))
+        # Second term of the loss is always zero since real_labels == 1
+        outputs = D(images)
+        D_loss_real = criterion(outputs, real_labels)
+        real_score = outputs
+        
+        # Compute BCELoss using fake images
+        # First term of the loss is always zero since fake_labels == 0
+        z = Variable(torch.randn(BATCH_SIZE, 64))
+        fake_images = G(z)
+        outputs = D(fake_images)
+        D_loss_fake = criterion(outputs, fake_labels)
+        fake_score = outputs
+        
+        # Backprop + Optimize
+        D_loss = D_loss_real + D_loss_fake
+        D.zero_grad()
+        D_loss.backward()
+        D_optimizer.step()
+
+        #=============== Train the generator ===============#
+        # Compute loss with fake images
+        z = Variable(torch.randn(BATCH_SIZE, 64))
+        fake_images = G(z)
+        outputs = D(fake_images)
+        
+        # We train G to maximize log(D(G(z)) instead of minimizing log(1-D(G(z)))
+        # For the reason, see the last paragraph of section 3. https://arxiv.org/pdf/1406.2661.pdf
+        G_loss = criterion(outputs, real_labels)
+        
+        # Backprop + Optimize
+        D.zero_grad()
+        G.zero_grad()
+        G_loss.backward()
+        G_optimizer.step()
+        
+        if step % 100 == 0:
+            print('Epoch: ', epoch, '| G train loss: %.4f | D train loss: %.4f' % (G_loss.data[0], D_loss.data[0]))
+
+            # plotting Generated image (second row)
+            z = Variable(torch.randn(BATCH_SIZE, 64))
+            fake_images = G(z)
+            for i in range(N_TEST_IMG):
+                a[1][i].clear()
+                a[1][i].imshow(np.reshape(fake_images.data.numpy()[i], (28, 28)), cmap='gray')
+                a[1][i].set_xticks(()); a[1][i].set_yticks(())
+            plt.draw(); plt.pause(0.05)
+
+plt.ioff()
+plt.show()
--- num0.py
+++ num0.py
@@ -88,17 +88,17 @@ optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)   # optimize all cnn param
 loss_func = nn.CrossEntropyLoss()                       # the target label is not one-hotted
 
 # following function (plot_with_labels) is for visualization, can be ignored if not interested
-from matplotlib import cm
-try: from sklearn.manifold import TSNE; HAS_SK = True
-except: HAS_SK = False; print('Please install sklearn for layer visualization')
-def plot_with_labels(lowDWeights, labels):
-    plt.cla()
-    X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
-    for x, y, s in zip(X, Y, labels):
-        c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
-    plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
+# from matplotlib import cm
+# try: from sklearn.manifold import TSNE; HAS_SK = True
+# except: HAS_SK = False; print('Please install sklearn for layer visualization')
+# def plot_with_labels(lowDWeights, labels):
+#     plt.cla()
+#     X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
+#     for x, y, s in zip(X, Y, labels):
+#         c = cm.rainbow(int(255 * s / 9)); plt.text(x, y, s, backgroundcolor=c, fontsize=9)
+#     plt.xlim(X.min(), X.max()); plt.ylim(Y.min(), Y.max()); plt.title('Visualize last layer'); plt.show(); plt.pause(0.01)
 
-plt.ion()
+# plt.ion()
 # training
 for epoch in range(EPOCH):
     for step, (x, y) in enumerate(train_loader):   # gives batch data, normalize x when iterate train_loader
@@ -118,14 +118,15 @@ for epoch in range(EPOCH):
             pred_y = torch.max(test_output, 1)[1].data.squeeze()
             accuracy = sum(pred_y == test_y) / float(test_y.size(0))
             print('Epoch: ', epoch, '| train loss: %.4f' % loss.data[0], '| test accuracy: %.2f' % accuracy)
-            if HAS_SK:
-                # Visualization of trained flatten layer (T-SNE)
-                tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
-                plot_only = 500
-                low_dim_embs = tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])
-                labels = test_y.numpy()[:plot_only]
-                plot_with_labels(low_dim_embs, labels)
-plt.ioff()
+#             if HAS_SK:
+#                 # Visualization of trained flatten layer (T-SNE)
+#                 tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=5000)
+#                 plot_only = 500
+#                 low_dim_embs = tsne.fit_transform(last_layer.data.numpy()[:plot_only, :])
+#                 labels = test_y.numpy()[:plot_only]
+#                 plot_with_labels(low_dim_embs, labels)
+# plt.ioff()
+# plt.show()
 # testing
 test_output, _ = cnn(test_x)
 pred_y = torch.max(test_output, 1)[1].data.numpy().squeeze()
